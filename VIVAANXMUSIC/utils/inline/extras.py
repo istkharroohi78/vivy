97045e2e2c13cd8100411a3dfadf036b0f0bcf51
ItsAnyaaa <anyasongbot@outlook.com>
2026-04-14 22:55:43 +0530

    Update extras.py

--- VIVAANXMUSIC/utils/inline/extras.py
+++ VIVAANXMUSIC/utils/inline/extras.py
@@ -1,14 +1,13 @@
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_CHAT
-from VIVAANXMUSIC.button_styles import danger_button, success_button
 
 
 def botplaylist_markup(_):
     buttons = [
         [
-            success_button(text=_["S_B_4"], url=SUPPORT_CHAT),
-            danger_button(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
@@ -18,7 +17,7 @@ def close_markup(_):
     upl = InlineKeyboardMarkup(
         [
             [
-                danger_button(
+                InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
                 ),
@@ -32,8 +31,8 @@ def supp_markup(_):
     upl = InlineKeyboardMarkup(
         [
             [
-                success_button(
-                    text=_["S_B_4"],
+                InlineKeyboardButton(
+                    text=_["S_B_9"],
                     url=SUPPORT_CHAT,
                 ),
             ]
