4678c873e692ad68f3723fedcd531e391dd670bf
istkharroohi78 <istkharroohi@gmail.com>
2026-07-06 21:03:20 +0530

    Update Spotify.py

--- VIVAANXMUSIC/platforms/Spotify.py
+++ VIVAANXMUSIC/platforms/Spotify.py
@@ -3,7 +3,7 @@ import re
 import httpx
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from youtubesearchpython.future import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 import config
 
